4afaa843d66f43b4b3bda10ea85265cf8428cf4a
haruhisayaguchi <hal.yaguchi@gmail.com>
2025-10-19 12:25:24 -0700

    [add] section_07

---
+++ section_07/advanced.ts
@@ -0,0 +1,20 @@
+type DataStore = {
+	[prop: string]: number | boolean;
+};
+
+// let someObj: Record<string, number | boolean>;
+
+let store: DataStore = {};
+
+store.id = 5;
+store.isOpen = false;
+// store.name = 'Max';
+
+let roles = ['admin', 'guest', 'editor'] as const;
+// roles.push('Max');
+const firstRole = roles[0];
+
+const dataEntries = {
+	entry1: 0.51,
+	entry2: -1.23
+} satisfies Record<string, number>;
